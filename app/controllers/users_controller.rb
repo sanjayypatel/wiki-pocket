bf35e35f05294504c27aeb695cd0f79f97cafc8b
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-06-25 15:57:15 -0700

    Formatted views for user sign in/out and password functions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+    else
+      flash[:error] = "Invalid user information"
+    end
+    redirect_to edit_user_registration_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+
+end
