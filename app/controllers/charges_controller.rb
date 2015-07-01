974037b177ed65ff78c3489c3c8f3d0f6c00e1ad
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-07-01 12:07:08 -0700

    Implemented ChargesController and test payment

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,42 @@
+class ChargesController < ApplicationController
+
+  def new
+    @stripe_btn_data = {
+      key: "#{Rails.configuration.stripe[:publishable_key]}",
+      description: "Wiki Pocket Premium Account Upgrade - #{current_user.username}",
+      amount: Amount.default,
+      email: current_user.email
+    }
+  end
+
+  def create
+
+    # Create Stripe Customer object
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    #Create a Stripe Charge object
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: Amount.default,
+      description: "Wiki Pocket Premium Account Upgrade - #{current_user.email}",
+      currency: 'usd'
+    )
+
+    flash[:success] = "Thanks for your payment, #{current_user.email}!"
+    redirect_to edit_user_registration_path(current_user)
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+  end
+
+  class Amount
+    def self.default
+      return 15_00
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   resources :wikis
-
+  resources :charges, only: [:new, :create]
   root to: 'welcome#index'
 
 end
