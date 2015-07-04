78c7c9f2a40e5333bc5b1f14240ef75f7a2f9812
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-07-03 21:02:36 -0700

    Private wikis created and tied to authors

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,21 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
 
-  scope :most_recently_updated, -> { order('updated_at DESC').limit(5)}
+  scope :most_recently_updated, -> { order('updated_at DESC') }
+  scope :only_private, -> { where(:private => true) }
+  after_initialize :init
+
+  def public?
+    !self.private
+  end
+
+  def make_public
+    self.update_attribute(:private, false)
+  end
+
+  def init
+    self.private = false if self.private.nil?
+  end
+
 end
+
