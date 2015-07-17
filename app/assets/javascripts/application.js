e0f8a58607d379941a07988043f8e8cc1ac5d308
Sanjay Y Patel <sanjay.y.patel@gmail.com>
2015-07-17 13:02:08 -0700

    BUG: JS snippet not creating events in RoRmetrics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,17 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+//= require event
+
+var _rm_event = {
+  name: "new name",
+}
+
+var _rm_request = new XMLHttpRequest();
+_rm_request.open("POST", "http://localhost:3000/api/events", true);
+_rm_request.setRequestHeader('Content-Type', 'application/json');
+_rm_request.onreadystatechange = function() {
+};
+
+
+_rm_request.send(JSON.stringify(_rm_event));
