f3cbb2fa1cff4c03c43d0f801d6f73d92bab3abf
Sanjay Patel <sanjay.y.patel@gmail.com>
2016-04-12 09:24:54 -0700

    Tag view styles

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,18 +3,14 @@ class TagsController < ApplicationController
   def show
     @tag = ActsAsTaggableOn::Tag.friendly.find(params[:id])
     tagged_wikis = Wiki.tagged_with(@tag.name)
-    tagged_links = Link.tagged_with(@tag.name)
     @wikis = tagged_wikis.select { |w| policy(w).show? }.paginate(page: params[:page], per_page: 5)
-    @links = tagged_links.select { |l| policy(l).show? }.paginate(page: params[:page], per_page: 5)
-
-    @active_tab = params[:tab] || "tagged-wikis"
   end
 
   def index
     if params[:tag_search]
       @tags = ActsAsTaggableOn::Tag.where('name LIKE ?', "%#{params[:tag_search]}%").paginate(page: params[:page], per_page: 5)
     else
-      @tags = ActsAsTaggableOn::Tag.most_used(100).paginate(page: params[:page], per_page: 5)
+      @tags = ActsAsTaggableOn::Tag.most_used(100).paginate(page: params[:page], per_page: 10)
     end
   end
 
